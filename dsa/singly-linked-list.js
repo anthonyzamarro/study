e6cd5eb5c884641ccc6eb060f5de8045fc11dc81
Anthony <anthony.zamarro@gmail.com>
2025-10-18 14:24:28 -0400

    add: more methods for singly linked list

--- dsa/singly-linked-list.js
+++ dsa/singly-linked-list.js
@@ -34,6 +34,7 @@ class SinglyLinkedList {
         return this
     }
 
+    // remove head
     shift() {
         if (!this.head) return undefined
         this.head = this.head.next
@@ -45,10 +46,21 @@ class SinglyLinkedList {
         return this.head
     }
 
-    insertAt() {
-
+    insertAt(index, val) {
+        if (index < 0 || index > this.length) return undefined
+        if (index === 0) return this.unshift(val)
+        if (index === this.length) return this.push(val)
+        
+        const node = new Node(val)
+        const prev = this.getIndex(index - 1)
+        const temp = prev.next
+        prev.next = node
+        node.next = temp
+        this.length++
+        return true
     }
 
+    // add to end of list
     push(val) {
         const node = new Node(val)
         if (!this.head) {
@@ -62,6 +74,7 @@ class SinglyLinkedList {
         return this
     }
 
+    // remove last item in list
     pop() {
         if (!this.head) {
             return undefined
@@ -96,9 +109,43 @@ class SinglyLinkedList {
             current = current.next
         }
     }
-    
-    removeAt() {
 
+    getIndex(index) {
+        if (index < 0 || index >= this.length) return undefined
+        let current = this.head
+        let currentIndex = 0
+        while (currentIndex !== index) {
+            current = current.next
+            currentIndex++
+        }
+        return current
+    }
+    
+    removeAt(index) {
+        if (index < 0 || index > this.length) return undefined
+        if (index === 0) return this.shift()
+        if (index === this.length) return this.pop()
+        
+        const prev = this.getIndex(index - 1)
+        const removed = prev.next
+        prev.next = removed.next
+        this.length--
+        return true
+    }
+
+    reverse() {
+        let node = this.head
+        this.head = this.tail
+        this.tail = node
+        let next
+        let prev = null
+        for(let i = 0; i < this.length; i++) {
+            next = node.next
+            node.next = prev
+            prev = node
+            node = next
+        }
+        return this
     }
 }
 
@@ -110,12 +157,14 @@ const list = new SinglyLinkedList()
 list.push(val1)
 list.push(val2)
 list.push(val3)
-list.unshift('prepend-head')
-list.unshift('prepend-head-AGAIN')
-list.pop() // remove val3
-list.shift() // remove prepend-head-AGAIN
+// list.unshift('prepend-head')
+// list.unshift('prepend-head-AGAIN')
+// list.pop() // remove val3
+// list.shift() // remove prepend-head-AGAIN
 list.push(val4)
+// list.insertAt(0, 'val5')
+// list.removeAt(2)
+// list.removeAt(0)
 
-
-console.log(list.getValue('val2'))
+console.log(list.reverse())
 
